0bea8e8eed30a5059fe84b31b2d677ffb4983d4b
edson <edson.cs.dev@gmail.com>
2021-08-14 16:27:57 -0300

    refactoring extract method

--- application/fastapi/routes/api.py
+++ application/fastapi/routes/api.py
@@ -16,23 +16,7 @@ def init_app(app, access_point="/api", encoding='utf-8'):
         """all elements on a table"""
         lines = file.file.readlines()
         file_name = file.filename.split(".")[0]
-        counter = 0
-        result = []
-        for i in lines:
-            if counter == 0:
-                table = Table(i.decode(encoding).split())
-                if not db[file_name]:
-                    table_repository = db.create_table(file_name,
-                                            primary_id='id',
-                                            primary_type=db.types.integer)
-                    db.commit()
-                else:
-                    table_repository = db[file_name]
-            else:
-                item = Row(counter, i.decode(encoding).split(), table.fields)
-                #table_repository.insert(item.__dict__)
-                result.append(item)
-            counter += 1
+        result, table_repository = await lines_to_object_list(file_name, lines)
         r = {"success": True}
         try:
             table_repository.insert_many([ob.__dict__ for ob in result])
@@ -44,5 +28,26 @@ def init_app(app, access_point="/api", encoding='utf-8'):
             })
         return r
         #return result
+
+    async def lines_to_object_list(file_name, lines):
+        db = dataset.connect("postgresql://postgres:123456@localhost:5432")
+        counter = 0
+        result = []
+        for i in lines:
+            if counter == 0:
+                table = Table(i.decode(encoding).split())
+                if not db[file_name]:
+                    table_repository = db.create_table(file_name,
+                                                       primary_id='id',
+                                                       primary_type=db.types.integer)
+                    db.commit()
+                else:
+                    table_repository = db[file_name]
+            else:
+                item = Row(counter, i.decode(encoding).split(), table.fields)
+                # table_repository.insert(item.__dict__)
+                result.append(item)
+            counter += 1
+        return result, table_repository
 
     return app
