ada80aa192217aa31010770d7c5f016e1e37ea8f
edson <edson.cs.dev@gmail.com>
2021-08-16 13:26:27 -0300

    basic tests structure #todo need split file location better and solve warnings mostly by pytest lib

--- application/fastapi/routes/api.py
+++ application/fastapi/routes/api.py
@@ -5,48 +5,46 @@ import dataset
 from classes.row import Row
 from classes.table import Table
 
-#define the database connection o start
+# define the database connection o start
 # use "localhost" or "postgres" when building docker
 env = "localhost"
 db = dataset.connect(f"postgresql://postgres:123456@{env}:5432")
 
 
-#innit app is a factory pattern that avoid ciclyc importation of the app
-#it split responsibility and use in that module only libs and services that it needs
+# innit app is a factory pattern that avoid ciclyc importation of the app
+# it split responsibility and use in that module only libs and services that it needs
 def init_app(app, access_point="/api", encoding='utf-8'):
-
-
-    #endpoint that recive the file upload
-    @app.post(access_point+"/upload_file", tags=[access_point])
+    # endpoint that recive the file upload
+    @app.post(access_point + "/upload_file", tags=[access_point])
     async def post_file(file: UploadFile = File(...)):
         """all lines in the file"""
         if file.filename == "":
             return {"error": "empty file"}
         lines = file.file.readlines()
-        #use filename w/o extesion for database name
+        # use filename w/o extesion for database name
         file_name = file.filename.split(".")[0]
         result, table_repository = await lines_to_object_list(file_name, lines)
         return_message = {"success": True}
-        #presist objects to database as a single insert many and in dictionary format
+        # presist objects to database as a single insert many and in dictionary format
         try:
             table_repository.insert_many([ob.__dict__ for ob in result])
         except Exception as e:
             raise HTTPException(status_code=409, detail={
                 "success": False,
-                 "error": str(e),
+                "error": str(e),
                 "type": "Conflict"
             })
         return return_message
 
     # simple route that return all the itens on database as a json format
-    ##todo optimize time with search restriction and cache usage since it's not change data
+    ##todo optimize time with search restriction and cache usage since it's not constatly changed data
     @app.get(access_point + "/table/{table}", tags=[access_point])
     async def get_table_itens(table, request: Request):
         return list(db[table].all()) if db[table] else {"error": "database not created"}
 
-    #transforma each line in a Row object
+    # transforma each line in a Row object
     async def lines_to_object_list(file_name, lines):
-        #reset database connection to avoid change data before rea data
+        # reset database connection to avoid change data before rea data
         db = dataset.connect(f"postgresql://postgres:123456@{env}:5432")
         counter = 0
         result = []
@@ -60,7 +58,7 @@ def init_app(app, access_point="/api", encoding='utf-8'):
                     table_repository = db[file_name]
             else:
                 item = Row(counter, i.decode(encoding).split(), table.fields)
-                # can try use insert one by one but time is a issue that way
+                # can try use insert one by one, but time is a issue that way
                 # table_repository.insert(item.__dict__)
                 result.append(item)
             counter += 1
